57153458c39b9d4f969cd75b008596ae20a4bd0a
yaoyao-ee <281649170+yaoyao-ee@users.noreply.github.com>
2026-07-26 17:09:05 +0800

    添加.gitignore，忽略编译产物和Keil用户配置文件

--- project/mdk/Objects/startup_mspm0g351x_uvision.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_mspm0g351x_uvision.o: ..\..\libraries\sdk\ti_config\startup_mspm0g351x_uvision.s
--- project/mdk/Objects/zf_device_type.d
+++
@@ -1,9 +0,0 @@
-./objects/zf_device_type.o: ..\..\libraries\zf_device\zf_device_type.c \
-  ..\..\libraries\zf_device\zf_device_type.h \
-  ..\..\libraries\zf_common\zf_common_typedef.h \
-  D:\keil\ 5\ARM\ARMCLANG\Bin\..\include\stdio.h \
-  D:\keil\ 5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  D:\keil\ 5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
-  D:\keil\ 5\ARM\ARMCLANG\Bin\..\include\stdarg.h \
-  D:\keil\ 5\ARM\ARMCLANG\Bin\..\include\string.h \
-  D:\keil\ 5\ARM\ARMCLANG\Bin\..\include\stdlib.h
