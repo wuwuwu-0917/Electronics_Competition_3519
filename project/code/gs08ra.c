8aedea15792b18914ec9d03bd48fe1db03d9a226
吴婉慈 <16186507+wu-wanci@user.noreply.gitee.com>
2026-07-30 10:37:37 +0800

    参数更正版本 赛道上记录时间为15.71s

--- project/code/gs08ra.c
+++ project/code/gs08ra.c
@@ -4,7 +4,7 @@
 // 光电管 最大最小值采集
 uint16 gray_max[6] = {0};
 uint16 gray_min[6] = {4095,4095,4095,4095,4095,4095};  // 12位ADC默认最大
-uint8  gray_threshold = 254;	  // 灰度阈值
+uint8  gray_threshold = 65;	  // 灰度阈值，只用于模拟量
 
 float deviation[6] = {0};     //误差赋值
 float turn_div = 0;           //总误差
@@ -30,6 +30,7 @@ void gray_max_min_reset(void)
         gray_min[i] = 4095;
     }
 }
+
 // 将采集的最大最小值直接写入 gs08ra_max_val / gs08ra_min_val
 void gray_save_max_min_to_array(void)
 {
@@ -46,10 +47,10 @@ void get_deviation(void)
     gs08ra_gpio_read();          //读取灰度传感器数据
   
     deviation[0] =  8*(!gs08ra_gpio_val[0]);      //黑为0，白为1，左负右正
-    deviation[1] =   3*(!gs08ra_gpio_val[1]);
+    deviation[1] =   4*(!gs08ra_gpio_val[1]);
     deviation[2] =   1*(!gs08ra_gpio_val[2]);
     deviation[3] =  -1*(!gs08ra_gpio_val[3]);
-    deviation[4] =  -3*(!gs08ra_gpio_val[4]);
+    deviation[4] =  -4*(!gs08ra_gpio_val[4]);     //这个参数基本没问题
     deviation[5] = -8*(!gs08ra_gpio_val[5]);
     
     turn_div = 0;    //误差清零
